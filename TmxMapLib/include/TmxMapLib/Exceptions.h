d311f5d381daad7d5b30cc5d07ee7d7613bdd916
Tony Slavotinek <apokryphos@users.noreply.github.com>
2018-04-11 14:33:05 -0400

    Formatting changes.

--- TmxMapLib/include/TmxMapLib/Exceptions.h
+++ TmxMapLib/include/TmxMapLib/Exceptions.h
@@ -5,35 +5,35 @@
 
 namespace TmxMapLib
 {
-    class NullArgumentException : public std::invalid_argument
-    {
-    public:
-        NullArgumentException(const std::string& argumentName)
-            : std::invalid_argument(argumentName) {
-        }
-    };
+class NullArgumentException : public std::invalid_argument
+{
+public:
+    NullArgumentException(const std::string& argumentName)
+        : std::invalid_argument(argumentName) {
+    }
+};
 
-    class XmlAttributeException : public std::runtime_error
-    {
-    public:
-        XmlAttributeException(const std::string& attributeName)
-            : std::runtime_error(attributeName) {
-        }
-    };
+class XmlAttributeException : public std::runtime_error
+{
+public:
+    XmlAttributeException(const std::string& attributeName)
+        : std::runtime_error(attributeName) {
+    }
+};
 
-    class XmlElementException : public std::runtime_error
-    {
-    public:
-        XmlElementException(const std::string& elementName)
-            : std::runtime_error(elementName) {
-        }
-    };
+class XmlElementException : public std::runtime_error
+{
+public:
+    XmlElementException(const std::string& elementName)
+        : std::runtime_error(elementName) {
+    }
+};
 
-    class XmlDocumentException : public std::runtime_error
-    {
-    public:
-        XmlDocumentException(const std::string& documentName)
-            : std::runtime_error(documentName) {
-        }
-    };
+class XmlDocumentException : public std::runtime_error
+{
+public:
+    XmlDocumentException(const std::string& documentName)
+        : std::runtime_error(documentName) {
+    }
+};
 }
